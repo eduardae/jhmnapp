9581c531e6cddc4b6859c21233000bbbb4921b22
Maurizio Ceglar <Mceglar@tacticalpeople.it>
2022-02-25 09:09:44 +0100

    Initial version of jhmnapp generated by generator-jhipster@7.6.0

---
+++ src/main/java/it/mc/jhmnapp/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package it.mc.jhmnapp.service.dto;
---
+++ src/test/java/it/mc/jhmnapp/IntegrationTest.java
@@ -0,0 +1,17 @@
+package it.mc.jhmnapp;
+
+import it.mc.jhmnapp.JhmnappApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = JhmnappApp.class)
+public @interface IntegrationTest {
+}
